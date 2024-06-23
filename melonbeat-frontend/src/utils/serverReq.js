865a15d4febf8ea3c328e67021fba417550c770b
elisei-florian-catalin <catalinelisei2002@gmail.com>
2024-06-23 18:59:18 +0300

    add backend url

--- melonbeat-frontend/src/utils/serverReq.js
+++ melonbeat-frontend/src/utils/serverReq.js
@@ -1,8 +1,98 @@
+// import axios from "axios";
+
+// export const makeUnauthenticatedPOSTRequest = async (route, body) => {
+//   try {
+//     const response = await axios.post(`http://localhost:5000/${route}`, body, {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error making POST request:", error);
+//     throw error;
+//   }
+// };
+
+// const getToken = () => {
+//   const accessToken = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, "$1");
+//   return accessToken;
+// };
+
+// export const makeAuthenticatedPOSTRequest = async (route, body) => {
+//   try {
+//     const token = getToken();
+//     const response = await axios.post(`http://localhost:5000/${route}`, body, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error making POST request:", error);
+//     throw error;
+//   }
+// };
+
+// export const makeAuthenticatedGETRequest = async (route) => {
+//   try {
+//     const token = getToken();
+//     const response = await axios.get(`http://localhost:5000/${route}`, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error making GET request:", error);
+//     throw error;
+//   }
+// };
+
+// export const makeAuthenticatedDELETERequest = async (route) => {
+//   try {
+//     const token = getToken();
+//     const response = await axios.delete(`http://localhost:5000/${route}`, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error making DELETE request:", error);
+//     throw error;
+//   }
+// };
+
+// export const makeAuthenticatedPUTRequest = async (route, body) => {
+//   try {
+//     const token = getToken();
+//     const response = await axios.put(`http://localhost:5000/${route}`, body, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error making PUT request:", error);
+//     throw error;
+//   }
+// };
+
 import axios from "axios";
 
 export const makeUnauthenticatedPOSTRequest = async (route, body) => {
   try {
-    const response = await axios.post(`http://localhost:5000/${route}`, body, {
+    const response = await axios.post(`https://melonbeat-backend.vercel.app/${route}`, body, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -23,7 +113,7 @@ const getToken = () => {
 export const makeAuthenticatedPOSTRequest = async (route, body) => {
   try {
     const token = getToken();
-    const response = await axios.post(`http://localhost:5000/${route}`, body, {
+    const response = await axios.post(`https://melonbeat-backend.vercel.app/${route}`, body, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
@@ -40,7 +130,7 @@ export const makeAuthenticatedPOSTRequest = async (route, body) => {
 export const makeAuthenticatedGETRequest = async (route) => {
   try {
     const token = getToken();
-    const response = await axios.get(`http://localhost:5000/${route}`, {
+    const response = await axios.get(`https://melonbeat-backend.vercel.app/${route}`, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
@@ -57,7 +147,7 @@ export const makeAuthenticatedGETRequest = async (route) => {
 export const makeAuthenticatedDELETERequest = async (route) => {
   try {
     const token = getToken();
-    const response = await axios.delete(`http://localhost:5000/${route}`, {
+    const response = await axios.delete(`https://melonbeat-backend.vercel.app/${route}`, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
@@ -74,7 +164,7 @@ export const makeAuthenticatedDELETERequest = async (route) => {
 export const makeAuthenticatedPUTRequest = async (route, body) => {
   try {
     const token = getToken();
-    const response = await axios.put(`http://localhost:5000/${route}`, body, {
+    const response = await axios.put(`https://melonbeat-backend.vercel.app/${route}`, body, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
