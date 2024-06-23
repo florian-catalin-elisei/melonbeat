02060336edbb8f961e633260d1642f7b7d4eff1a
elisei-florian-catalin <catalinelisei2002@gmail.com>
2024-06-23 18:54:42 +0300

    add cors origin

--- melonbeat-backend/index.js
+++ melonbeat-backend/index.js
@@ -12,7 +12,11 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://melonbeat-backend.vercel.app",
+  })
+);
 app.use(express.json());
 
 app.use("/api/getTrackDetails", async (req, res) => {
