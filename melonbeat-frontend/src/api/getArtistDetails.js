376e134826a96a1dc5ad1dcdaac76ed8b58f4a9d
elisei-florian-catalin <catalinelisei2002@gmail.com>
2024-06-24 14:09:10 +0300

    fix error

--- melonbeat-frontend/src/api/getArtistDetails.js
+++ melonbeat-frontend/src/api/getArtistDetails.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api/getArtistDetails",
-  // baseURL: "https://melonbeat-backend.vercel.app/api/getArtistDetails",
+  // baseURL: "http://localhost:5000/api/getArtistDetails",
+  baseURL: "https://melonbeat-backend.vercel.app/api/getArtistDetails",
   headers: {
     "X-RapidAPI-Key": "c17456ddbemsh6933506b8420aa7p1dc3bfjsn24cc82c4835a",
     "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
