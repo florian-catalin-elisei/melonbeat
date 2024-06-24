275390df9996723a132ad422ff80323d3dc81274
elisei-florian-catalin <catalinelisei2002@gmail.com>
2024-06-24 14:35:32 +0300

    fix error

--- melonbeat-backend/index.js
+++ melonbeat-backend/index.js
@@ -12,6 +12,14 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://melonbeat-frontend.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(
   cors({
     origin: "https://melonbeat-frontend.vercel.app",
